bf9fff0f7898f4b00e722881b4a6ec4eeaa5b06a
Alexander Kondenko <kondenko.a@gmail.com>
2016-08-19 08:59:44 +0300

    add fabric

--- app/src/main/java/net/ginapps/myphonenumber/MyPhoneApplication.java
+++ app/src/main/java/net/ginapps/myphonenumber/MyPhoneApplication.java
@@ -3,6 +3,8 @@ package net.ginapps.myphonenumber;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 public class MyPhoneApplication extends Application{
@@ -10,6 +12,7 @@ public class MyPhoneApplication extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         Timber.plant(new Timber.DebugTree());
     }
 
